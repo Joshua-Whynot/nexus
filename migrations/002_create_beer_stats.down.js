66ccaf3d3d2adc71bfe0e4bf9ec7d3f3604b3b1a
Joshua Whynot <joshuawhynot@gmail.com>
2026-08-15 04:29:57 +0000

    Added migration 2

--- migrations/002_create_beer_stats.down.js
+++ migrations/002_create_beer_stats.down.js
@@ -5,14 +5,14 @@ const dbPath = path.join(process.cwd(), 'data', 'beers.db');
 const db = new Database(dbPath);
 
 try {
-  db.exec('BEGIN');
-  db.exec('DROP TABLE IF EXISTS beer_stats');
-  db.exec('COMMIT');
-  console.log('beer_stats table dropped');
+    db.exec('BEGIN');
+    db.exec('DROP TABLE IF EXISTS beer_stats');
+    db.exec('COMMIT');
+    console.log('beer_stats table dropped');
 } catch (err) {
-  console.error('Down migration 002 failed:', err);
-  try { db.exec('ROLLBACK'); } catch (e) {}
-  process.exit(1);
+    console.error('Down migration 002 failed:', err);
+    try { db.exec('ROLLBACK'); } catch (e) { }
+    process.exit(1);
 } finally {
-  db.close();
+    db.close();
 }
